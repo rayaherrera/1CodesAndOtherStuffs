d11ead8e01c3be235897e43a384dc814b5465049
rayaherrera <rayaherrera@aol.com>
2019-10-24 08:37:45 -0500

    Some repetition of words and such.

---
+++ StringsAndThings.py
@@ -0,0 +1,25 @@
+# Strings
+#    data that falls within " " marks
+
+# Concatenation
+#  Put 2 or more strings together
+
+firstName = "Fred"
+lastName = "Flinstone"
+
+fullName = firstName + " " + lastName
+
+print(fullName)
+
+# Repetition
+#  repetition operator: *
+
+print("Hip "*2 + "Hooray!")
+
+def rowYourBoat():
+    print("Row, "*3 + 'your boat')
+    print("Gently down the stream")
+    print("Merrily, "*4)
+    print("Life is but a dream")
+
+rowYourBoat()
