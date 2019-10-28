248e0a8ebc86111eae2f4ee540cd24557a96644f
rayaherrera <rayaherrera@aol.com>
2019-10-28 08:22:07 -0500

    I was able to finish the lecture by looking at Mr. Blacks code online

--- StringsAndThings.py
+++ StringsAndThings.py
@@ -23,3 +23,49 @@ def rowYourBoat():
     print("Life is but a dream")
 
 rowYourBoat()
+
+# Indexing
+
+name = "Roy G Biv"
+firstChar = name[0]
+print(firstChar)
+
+middleCharIndex = len(name) // 2
+print(middleCharIndex)
+print(name[middleCharIndex])
+
+print(name[-3])
+
+for i in range(0, len(name)):
+    print(name[i])
+
+# Slicing and dicing
+
+print(name[-4:8])
+
+for i in range(0, len(name)+1):
+    print(name[0:i])
+
+# Searching
+
+print("biv" in name)
+
+if "y" not in name:
+    print("y is not in name")
+else:
+    print("y is in name")
+
+    # String Methods to investigate:
+    # Method        Use Example         Explanation
+    # center        aStr.center(w)
+    # ljust         aStr.ljust(w)
+    # rjust         aStr.rjust(w)
+    # upper         aStr.upper()
+    # lower         aStr.lower()
+    # index         aStr.index(item)
+    # rindex        aStr.rindex(item)
+    # find          aStr.find(item)
+    # rfind         aStr.rfind(item)
+    # replace       aStr.replace(old, new)
+
+    # Be sure to include multiple examples of all of them in use
