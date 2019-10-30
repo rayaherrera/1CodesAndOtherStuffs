3f55b6809b9e25f1eb566db192207a6080769c42
rayaherrera <rayaherrera@aol.com>
2019-10-30 08:34:18 -0500

    Finished my crypto lecture code

--- mapper.py
+++ mapper.py
@@ -1,1 +1,20 @@
 def letterToIndex(letter):
+    from string import ascii_lowercase
+    alphabet = ascii_lowercase + ' '
+    idx = alphabet.find(letter)
+    if idx == -1:  # not found
+        print("error:", letter, "is not in the alphabet")
+    return idx
+
+
+def indexToLetter(idx):
+    from string import ascii_lowercase
+    alphabet = ascii_lowercase + ' '
+    letter = ''
+    if idx >= len(alphabet):
+        print("error:", idx, "is too large")
+    elif idx < 0:
+        print("error:", idx, "is too small")
+    else:
+        letter = alphabet[idx]
+    return letter
---
+++ venv/crypto.py
@@ -0,0 +1,38 @@
+# Transposition Cipher
+
+# original: this_is_a_secret_message_that_i_want_to_transmit
+# encrypted:hsi__ertmsaeta__att_rnmtti_sasce_esg_htiwn_otasi
+
+def scramble2Encrypt(plainText):
+    evenChars = ""
+    oddChars = ""
+    charCount = 0
+    for ch in plainText:
+        if charCount % 2 == 0:
+            evenChars = evenChars + ch
+        else:
+            oddChars = oddChars + ch
+        charCount = charCount + 1
+    cipherText = oddChars + evenChars
+    return cipherText
+
+def scramble2Decrypt(cipherText):
+    halfLength = len(cipherText) // 2
+    evenChars = cipherText[halfLength:]
+    oddChars = cipherText[:halfLength]
+    plainText = ""
+
+    for i in range(halfLength):
+        plainText = plainText + evenChars[i]
+        plainText = plainText + oddChars[i]
+
+    if len(oddChars) < len(evenChars):
+        plainText = plainText + evenChars[-1]
+
+    return plainText
+
+
+def encryptMessage():
+    msg = input("Enter the message to encrypt: ")
+    cipherText = scramble2Encrypt(msg)
+    print("The encrypted message is:", cipherText)
